ca6e4a4da9bd57d2c2216a72657d2beaa2c20323
Yeasin <career3517@gmail.com>
2024-10-08 09:29:38 -0400

    single data test case

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,20 @@
+package api.endpoints;
+//Swagger URI------->https://petstore.swagger.io
+//endspoints--->user,user/{username},
+public class Routes {
+	
+	public static String base_url = "https://petstore.swagger.io/v2";
+	
+	//USER Model
+	public static String post_url = base_url+"/user";
+	public static String get_url = base_url+"/user/{username}";
+	public static String update_url = base_url+"/user/{username}";
+	public static String delete_url = base_url+"/user/{username}";
+	
+	//store module
+	//here store module urls
+	//pet modulue
+	//pet module urls
+	
+
+}
